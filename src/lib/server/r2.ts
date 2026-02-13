b8a77cd88c6e4a51651c4a320d09427532a5520d
shisole <stephen.hisole@avb.net>
2026-02-13 14:57:14 +0800

    wip: Add path style

--- src/lib/server/r2.ts
+++ src/lib/server/r2.ts
@@ -6,6 +6,7 @@ function getR2Client() {
 	return new S3Client({
 		region: 'auto',
 		endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+		forcePathStyle: true,
 		credentials: {
 			accessKeyId: env.R2_ACCESS_KEY_ID!,
 			secretAccessKey: env.R2_SECRET_ACCESS_KEY!
